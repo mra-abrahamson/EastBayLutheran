d08907d9d0fc0c5b6dd719ff1888b2ff39fcdceb
Matt Abrahamson <mra.abrahamson@gmail.com>
2019-03-31 14:03:33 -0700

    bg transition to new EBLP collage

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,24 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Welcome to the <br />East Bay Lutheran Parish</h1>
+                <p>We are a non-profit ELCA organization sponsored by congregations in the East Bay, San Francisco Bay Area, California. We provide pastoral care, mentoring and education, inspire volunteers, steward our resources responsibly and maintain a meaningful Lutheran presence through service in our community.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>About Us</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Volunteer</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Donate</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Education</a></li>
+            </ul>
+        </nav>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>EBLP</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>LMNH</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>EBLYP</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact Us</a></li>
             </ul>
         </nav>
     </header>
